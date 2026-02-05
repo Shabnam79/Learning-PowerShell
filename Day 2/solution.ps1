1f622934fb36de739789aec0840d6115b9e25777
Shabnam79 <109502045+Shabnam79@users.noreply.github.com>
2026-02-05 22:40:03 +0530

    Add PowerShell script for experience output

---
+++ Day 2/solution.ps1
@@ -0,0 +1,5 @@
+$name="shabnam"
+$No_of_exp=4
+$increment =2
+$after_two_years=$No_of_exp + $increment
+write-output "My name is $name and I have $No_of_exp after 2 years I will have $after_two_years exp"
